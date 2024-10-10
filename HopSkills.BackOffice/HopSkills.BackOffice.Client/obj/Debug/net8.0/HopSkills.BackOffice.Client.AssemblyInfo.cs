d61bd093e9ac73a2f495527abfb646f71a2a8098
Samy Dakhlaoui <s.dakhlaoui@vir.fr>
2024-10-10 19:23:01 +0200

    claude

--- HopSkills.BackOffice/HopSkills.BackOffice.Client/obj/Debug/net8.0/HopSkills.BackOffice.Client.AssemblyInfo.cs
+++ HopSkills.BackOffice/HopSkills.BackOffice.Client/obj/Debug/net8.0/HopSkills.BackOffice.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HopSkills.BackOffice.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3187d729a1fdb2a66665e333f2d6fb8987824c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0383068b6449a9d60a05feabd1a1976a63814ff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HopSkills.BackOffice.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HopSkills.BackOffice.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
